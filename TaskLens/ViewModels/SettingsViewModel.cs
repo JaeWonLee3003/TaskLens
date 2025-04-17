faefc20d9f1b695f9a9b4ae6f0ce35ccf3f172d3
재원 이 <kenneth.lee@latisglobal.net>
2025-04-17 18:46:25 +0900

    설정 화면 바인딩 일부 구현

--- TaskLens/ViewModels/SettingsViewModel.cs
+++ TaskLens/ViewModels/SettingsViewModel.cs
@@ -1,12 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using TaskLens.Core;
 
 namespace TaskLens.ViewModels
 {
-    internal class SettingsViewModel
+    public class SettingsViewModel : INotifyPropertyChanged
     {
+        private bool _autoStart;
+        private bool _enableCpuWarning;
+        private bool _enableRamWarning;
+
+        public bool AutoStart
+        {
+            get => _autoStart;
+            set { _autoStart = value; OnPropertyChanged(); }
+        }
+
+        public bool EnableCpuWarning
+        {
+            get => _enableCpuWarning;
+            set { _enableCpuWarning = value; OnPropertyChanged(); }
+        }
+
+        public bool EnableRamWarning
+        {
+            get => _enableRamWarning;
+            set { _enableRamWarning = value; OnPropertyChanged(); }
+        }
+
+        public ICommand SaveSettingsCommand { get; }
+
+        public SettingsViewModel()
+        {
+            // 임시 초기값
+            AutoStart = false;
+            EnableCpuWarning = true;
+            EnableRamWarning = true;
+
+            SaveSettingsCommand = new RelayCommand(o => SaveSettings());
+        }
+
+        private void SaveSettings()
+        {
+            // TODO: 실제 저장 로직 연결
+            System.Windows.MessageBox.Show(
+    "설정이 성공적으로 저장되었습니다.",
+    "설정 저장 완료",
+    System.Windows.MessageBoxButton.OK,
+    System.Windows.MessageBoxImage.Information
+);
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
