6d838dc33cad781ed65990b796bf6886c2136fed
재원 이 <kenneth.lee@latisglobal.net>
2025-05-02 09:20:35 +0900

    테마 반영 및 경고 로그 업데이트

---
+++ TaskLens/Helpres/IconHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Management;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace TaskLens.Helpers
+{
+    public static class IconHelper
+    {
+        public static ImageSource GetProcessIcon(string exePath)
+        {
+            try
+            {
+                Icon icon = Icon.ExtractAssociatedIcon(exePath);
+                return Imaging.CreateBitmapSourceFromHIcon(
+                    icon.Handle,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromWidthAndHeight(20, 20));
+            }
+            catch
+            {
+                return null;
+            }
+
+
+        }
+
+        public static string GetProcessPathSafe(string processName)
+        {
+            try
+            {
+                var searcher = new ManagementObjectSearcher(
+                    $"SELECT ExecutablePath FROM Win32_Process WHERE Name = '{processName}.exe'");
+                foreach (var obj in searcher.Get())
+                {
+                    var pathObj = obj["ExecutablePath"];
+                    if (pathObj != null)
+                        return pathObj.ToString();
+                }
+            }
+            catch { }
+            return string.Empty;
+        }
+    }
+}
---
+++ TaskLens/Helpres/UIHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace TaskLens.Helpers
+{
+    public static class UIHelpers
+    {
+        public static T FindVisualParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            while (child != null)
+            {
+                if (child is T parent)
+                    return parent;
+
+                child = System.Windows.Media.VisualTreeHelper.GetParent(child);
+            }
+            return null;
+        }
+    }
+}
